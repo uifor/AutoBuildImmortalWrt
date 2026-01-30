1c189e8d6217650b0eee187430f708372b043186
uifor <uifor@qq.com>
2026-01-30 19:19:34 +0800

    Change git clone URL for store repository
    Updated the repository URL for cloning third-party software.

--- n1/build.sh
+++ n1/build.sh
@@ -43,7 +43,7 @@ PACKAGES="$PACKAGES perlbase-base perlbase-file perlbase-time perlbase-utf8 perl
 CUSTOM_PACKAGES="$CUSTOM_PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 
 echo "🔄 正在同步第三方软件仓库 Cloning run file repo..."
-git clone --depth=1 https://github.com/wukongdaily/store.git /tmp/store-run-repo
+git clone --depth=1 https://github.com/uifor/store.git /tmp/store-run-repo
 # 拷贝 run/arm64 下所有 run 文件和ipk文件 到 extra-packages 目录
 mkdir -p /home/build/immortalwrt/extra-packages
 cp -r /tmp/store-run-repo/run/arm64/* /home/build/immortalwrt/extra-packages/
